4d2c7a99dfcbe4eb53b183d9c1cf1cc9f076cda1
luismurillo <12983430+neuromurillo@users.noreply.github.com>
2026-01-25 17:53:06 -0600

    Se pasó la clave de brevo a la variable de entorno en .env, se crearon .env de respaldo para _main y _dev; y se conectó la base de datos de develop a certificados_db_dev

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "certificados_db");
+$conexion = new mysqli("localhost", "root", "", "certificados_db_dev");
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
